7d0288a1e9f7eb282a54e1698bcddd980648db16
zephkelly <masterevank@gmail.com>
2025-03-31 20:36:44 +1100

    Change cookie domain

--- main/nuxt.config.ts
+++ main/nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
             name: 'paincoach-session',
             password: process.env.NUXT_SESSION_PASSWORD || '',
             cookie: {
-                domain: process.env.ENVIRONMENT_DOMAIN,
+                domain: process.env.ENVIRONMENT_COOKIE_DOMAIN,
                 sameSite: 'lax'
             }
         },
